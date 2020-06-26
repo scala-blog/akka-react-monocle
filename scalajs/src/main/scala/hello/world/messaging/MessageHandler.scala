666215c9c515f600344c575d6a65d2dbb3e96089
hernan saab <hernansaab@hernans-MacBook-Pro.local>
2020-06-25 19:20:41 -0700

    preserve old copy commented

--- scalajs/src/main/scala/hello/world/messaging/MessageHandler.scala
+++ scalajs/src/main/scala/hello/world/messaging/MessageHandler.scala
@@ -26,10 +26,12 @@ object MessageHandler {
     private def messageUpdate: PartialFunction[Any, Application] = {
       case IncrementCounter1 =>
         log.info(s"Increment  counter1")
+        //models.topModel.app.copy(counter1 = Counter(models.topModel.app.counter1.value + 1))
         counter1Lens.modify(c => c.copy(c.value+1))(models.topModel.app)
 
       case IncrementCounter2 =>
         log.info(s"Increment counter2")
+        //models.topModel.app.copy(counter2 = Counter(models.topModel.app.counter2.value + 1))
         counter2Lens.modify(c => c.copy(c.value+1))(models.topModel.app)
     }
   }
